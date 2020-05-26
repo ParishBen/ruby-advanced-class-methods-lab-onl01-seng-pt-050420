0147217d49aabaca0047367bc02de6b4ec641b27
Ben Parish <parishben13@gmail.com>
2020-05-26 03:30:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,33 @@ class Song
   end
 
   def save
-    self.class.all << self
+    self.all << self
+   end
+  def Song.create
+  song = self.new
+  self.all << song
+  song
+end
+  def Song.new_by_name(name)
+song= self.new
+song.name = name
+song
   end
-
+ def Song.create_by_name(name)
+  song = self.new
+  song.name = name
+  self.all << song
+  song
+ end
+ def Song.find_by_name(name)
+   self.all.find{|person| person.name == name}
+end
+def Song.find_or_create_by_name(name)
+  if Song.find_by_name(name)
+  name
+  else
+    name = self.new
+    name
+   end
+end
 end
